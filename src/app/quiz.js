808823380ab33415ffa809115175489fd48c74b6
Marko Goronjic <goronjicmarko24@gmail.com>
2021-07-15 09:48:56 +0200

    Finished javascript quiz

--- src/app/quiz.js
+++ src/app/quiz.js
@@ -1,38 +1,104 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-param-reassign */
 
 export class Quiz {
   constructor(questions) {
     this.questions = questions;
     this.index = 0;
     this.score = 0;
+    this.maxScore = 100;
     this.questionContainer = document.querySelectorAll('.js-questionContainer');
+    this.multiQuestionContainer = document.querySelectorAll('.js-multiQuestionContainer');
+    this.multiAnswerText = document.querySelector('.js-multiAnswerText');
+    this.answerText = document.querySelector('.js-answerText');
+    this.inputQuestionContainer = document.querySelector('.js-inputQuestionContainer');
+    this.inputAnswerText = document.querySelector('.js-inputAnswerText');
+    this.mainResult = document.querySelector('.js-mainResult');
+    this.passContainer = document.querySelector('.js-passContainer');
+    this.failContainer = document.querySelector('.js-failContainer');
   }
 
   getQuestions() {
-    console.log(this.questions[this.index]);
     return this.questions[this.index];
   }
 
+  getOptions() {
+    return this.questions[this.index].answers;
+  }
+
   userAnswer(answers, target) {
-    if (answers === this.getQuestions().answer) {
+    if (answers === 'true') {
       this.score += this.getQuestions().points;
-      for (let index = 0; index < this.questionContainer.length; index += 1) {
-        target.classList.add('correct');
-      }
-      console.log(this.score);
+      target.classList.add('correct');
     } else {
       target.classList.add('wrong');
       this.score += 0;
     }
   }
 
-  nextQuestion() {
-    this.index += 1;
-    this.questionContainer.forEach((element) => {
-      element.classList.remove('wrong');
-      element.classList.remove('correct');
-    });
+  disableBtns(buttons) {
+    for (let index = 0; index < buttons.length; index += 1) {
+      buttons[index].setAttribute('disabled', true);
+    }
+  }
+
+  enableBtns(buttons) {
+    for (let index = 0; index < buttons.length; index += 1) {
+      buttons[index].removeAttribute('disabled');
+    }
+  }
+
+  nextQuestion(question) {
+    if (question.checked && this.getQuestions().isCheckInput === false) {
+      this.questionContainer.forEach((quesContainer) => {
+        quesContainer.classList.remove('wrong');
+        quesContainer.classList.remove('correct');
+        this.answerText.classList.remove('active');
+      });
+    } else if (question.checked && this.getQuestions().isCheckInput === true) {
+      this.multiQuestionContainer.forEach((multiQues) => {
+        multiQues.classList.remove('wrong');
+        multiQues.classList.remove('correct');
+        this.multiAnswerText.classList.remove('active');
+      });
+    } else if (this.getQuestions().inputQuestion === true) {
+      this.inputQuestionContainer.classList.remove('wrong');
+      this.inputQuestionContainer.classList.remove('correct');
+      this.inputAnswerText.classList.remove('active');
+    }
+  }
+
+  checkInputAnswer(input, target) {
+    if (input === this.getQuestions().answer) {
+      this.score += this.getQuestions().points;
+      target.classList.add('correct');
+    } else {
+      this.score += 0;
+      target.classList.add('wrong');
+      this.inputAnswerText.classList.add('active');
+      this.inputAnswerText.innerHTML = `The correct answer is: ${this.getQuestions().answer}`;
+    }
+  }
+
+  userScore(targetPass, targetFail) {
+    this.mainResult.classList.add('active');
+    if (this.score > 75) {
+      targetPass.innerHTML = `You have passed :) ! Congratulations you have scored ${this.score} out of ${this.maxScore}.`;
+      this.passContainer.classList.add('active');
+    } else {
+      targetFail.innerHTML = `You have failed :( ! You have scored ${this.score} out of ${this.maxScore}, better luck next time!`;
+      this.failContainer.classList.add('active');
+    }
+  }
+
+  questionAnimation(target) {
+    target.classList.add('slideUpBottom');
+    setTimeout(() => {
+      target.classList.remove('slideUpBottom');
+      target.classList.remove('slideUp');
+    }, 2000);
   }
 
   end() {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable one-var */
 
 import '../scss/style.scss';
 import { pageLoader } from './app/webpage-loader';
@@ -8,57 +9,199 @@ import { questions } from './app/questions';
 pageLoader();
 
 (function () {
-  const question = document.querySelector('.js-question');
-  const questionText = document.querySelectorAll('.js-questionText');
-  const radioBtns = document.querySelectorAll('.js-radioBtn');
-  const confirmBtnHolder = document.querySelector('.js-confirmBtnHolder');
-  const oneAnswerQuestions = document.querySelector('.js-oneAnswerQuestions');
-  const mainStartQuiz = document.querySelector('.js-mainStartQuiz');
-  const nextBtn = document.querySelector('.js-nextBtn');
-  const category = document.querySelector('.js-category');
+  const question = document.querySelector('.js-question'),
+    questionText = document.querySelectorAll('.js-questionText'),
+    radioBtns = document.querySelectorAll('.js-radioBtn'),
+    confirmBtnHolder = document.querySelector('.js-confirmBtnHolder'),
+    oneAnswerQuestions = document.querySelector('.js-oneAnswerQuestions'),
+    multiAnsQuestion = document.querySelector('.js-multiAnsQuestion'),
+    multiOption = document.querySelectorAll('.js-multiOption'),
+    multiQuestion = document.querySelector('.js-multiQuestion'),
+    checkBox = document.querySelectorAll('.js-checkBox'),
+    mainStartQuiz = document.querySelector('.js-mainStartQuiz'),
+    nextBtn = document.querySelector('.js-nextBtn'),
+    category = document.querySelector('.js-category'),
+    multiCategory = document.querySelector('.js-multiCategory'),
+    inputQuestions = document.querySelector('.js-inputQuestions'),
+    inputQuestion = document.querySelector('.js-inputQuestion'),
+    inputCategory = document.querySelector('.js-inputCategory'),
+    answerInput = document.querySelector('.js-answerInput'),
+    mainHeader = document.querySelector('.js-mainHeader'),
+    confirmBtn = document.querySelector('.js-confirmBtn'),
+    quiz = new Quiz(questions);
 
-  const quiz = new Quiz(questions);
+  function formatString(string) {
+    return string
+      .replace(/(\B)[^ ]*/g, (match) => (match.toLowerCase()))
+      .replace(/^[^ ]/g, (match) => (match.toUpperCase()));
+  }
 
   document.body.addEventListener('click', (e) => {
     const targetBlock = e.target;
     function run() {
       if (!quiz.end()) {
         const currentQuestion = quiz.getQuestions();
-        question.innerHTML = currentQuestion.text;
-        category.innerHTML = `Category: ${currentQuestion.category}`;
+        const quesOptions = quiz.getOptions();
 
-        for (let index = 0; index < questionText.length; index += 1) {
-          questionText[index].innerHTML = currentQuestion.options[index];
-          radioBtns[index].setAttribute('value', questionText[index].innerHTML);
-        };
+        if (currentQuestion.inputQuestion === false) {
+          if (question && category) {
+            question.innerHTML = currentQuestion.text;
+            category.innerHTML = `Category: ${currentQuestion.category}`;
+          }
+
+          for (let index = 0; index < quesOptions.length; index += 1) {
+            if (questionText && radioBtns) {
+              questionText[index].innerHTML = quesOptions[index].option;
+              radioBtns[index].setAttribute('value', questionText[index].innerHTML);
+              radioBtns[index].setAttribute('correct', quesOptions[index].correct);
+            }
+          }
+          if (currentQuestion.isCheckInput === true) {
+            if (oneAnswerQuestions && multiAnsQuestion) {
+              oneAnswerQuestions.classList.remove('active');
+              multiAnsQuestion.classList.add('active');
+            }
+            if (multiQuestion) {
+              multiQuestion.innerHTML = `${currentQuestion.text} *select two answers`;
+              multiCategory.innerHTML = `Category: ${currentQuestion.category}`;
+            }
+            for (let i = 0; i < multiOption.length; i += 1) {
+              if (multiOption && checkBox) {
+                multiOption[i].innerHTML = quesOptions[i].option;
+                checkBox[i].setAttribute('value', multiOption[i].innerHTML);
+                checkBox[i].setAttribute('correct', quesOptions[i].correct);
+              }
+            }
+          }
+        } else if (currentQuestion.inputQuestion === true) {
+          if (multiAnsQuestion && inputQuestion) {
+            multiAnsQuestion.classList.remove('active');
+            inputQuestions.classList.add('active');
+            inputQuestion.innerHTML = currentQuestion.text;
+          }
+          if (inputCategory && answerInput) {
+            inputCategory.innerHTML = `Category: ${currentQuestion.category}`;
+            answerInput.setAttribute('data', currentQuestion.answer);
+          }
+        }
       } else {
-        console.log(`kraj ${quiz.score}`);
+        const quizPass = document.createElement('h1');
+        const quizFail = document.createElement('h1');
+        if (quiz) {
+          quiz.passContainer.appendChild(quizPass);
+          quiz.failContainer.appendChild(quizFail);
+        }
+        if (confirmBtnHolder && inputQuestions) {
+          confirmBtnHolder.classList.remove('active');
+          inputQuestions.classList.remove('active');
+        }
+        quiz.userScore(quizPass, quizFail);
       }
     }
     if (e.target.classList.contains('js-confirmBtn')) {
-      nextBtn.classList.add('active');
-      radioBtns.forEach((radioBtn) => {
-        if (radioBtn.checked === true) {
-          console.log(true);
-          quiz.userAnswer(radioBtn.getAttribute('value'), radioBtn.parentNode);
+      const currentQuestion = quiz.getQuestions();
+      setTimeout(() => {
+        if (nextBtn) {
+          nextBtn.classList.add('active');
         }
-      });
+      }, 3000);
+      if (confirmBtn) {
+        confirmBtn.classList.remove('active');
+      }
+      if (currentQuestion.isCheckInput === false && currentQuestion.inputQuestion === false) {
+        if (oneAnswerQuestions && multiAnsQuestion) {
+          oneAnswerQuestions.classList.add('slideUp');
+          multiAnsQuestion.classList.add('slideUpBottom');
+        }
+        if (quiz && radioBtns) {
+          quiz.questionAnimation(multiAnsQuestion);
+          radioBtns.forEach((radioBtn) => {
+            if (radioBtn.checked === true) {
+              quiz.disableBtns(radioBtns);
+              quiz.userAnswer(radioBtn.getAttribute('correct'), radioBtn.parentNode);
+            }
+          });
+        }
+      } else if (currentQuestion.isCheckInput === true && currentQuestion.inputQuestion === false) {
+        if (multiAnsQuestion && inputQuestions) {
+          multiAnsQuestion.classList.add('slideUp');
+          inputQuestions.classList.add('slideUpBottom');
+        }
+        if (quiz && checkBox) {
+          quiz.questionAnimation(inputQuestions);
+          checkBox.forEach((box) => {
+            if (box.checked === true) {
+              quiz.disableBtns(checkBox);
+              quiz.userAnswer(box.getAttribute('correct'), box.parentNode);
+            }
+          });
+        }
+      } else if (currentQuestion.inputQuestion === true) {
+        if (inputQuestion && answerInput) {
+          inputQuestions.classList.add('slideUp');
+          answerInput.setAttribute('disabled', true);
+          quiz.checkInputAnswer(formatString(answerInput.value), answerInput.parentNode);
+        }
+      }
     }
 
     if (targetBlock === nextBtn) {
-      run();
-      quiz.nextQuestion();
-      nextBtn.classList.remove('active');
-      for (let index = 0; index < radioBtns.length; index += 1) {
-        radioBtns[index].checked = false;
+      const currentQuestion = quiz.getQuestions();
+      if (nextBtn && confirmBtn) {
+        nextBtn.classList.remove('active');
+        confirmBtn.classList.remove('active');
+        setTimeout(() => {
+          confirmBtn.classList.add('active');
+        }, 2000);
       }
+      if (currentQuestion.isCheckInput === false && currentQuestion.inputQuestion === false) {
+        if (quiz && radioBtns) {
+          quiz.questionAnimation(oneAnswerQuestions);
+          for (let index = 0; index < radioBtns.length; index += 1) {
+            quiz.nextQuestion(radioBtns[index]);
+            radioBtns[index].checked = false;
+            quiz.enableBtns(radioBtns);
+          }
+        }
+      } else if (currentQuestion.isCheckInput === true && currentQuestion.inputQuestion === false) {
+        if (quiz && checkBox) {
+          quiz.questionAnimation(multiAnsQuestion);
+          for (let i = 0; i < checkBox.length; i += 1) {
+            quiz.nextQuestion(checkBox[i]);
+            checkBox[i].checked = false;
+            quiz.enableBtns(checkBox);
+          }
+        }
+      } else if (currentQuestion.inputQuestion === true) {
+        if (quiz && answerInput) {
+          quiz.questionAnimation(inputQuestions);
+          answerInput.removeAttribute('disabled');
+          quiz.nextQuestion(answerInput);
+          answerInput.value = '';
+        }
+        quiz.index += 1;
+      }
+      run();
     }
 
     if (targetBlock.classList.contains('js-startQuiz')) {
       run();
-      mainStartQuiz.classList.add('hidden');
-      oneAnswerQuestions.classList.add('active');
-      confirmBtnHolder.classList.add('active');
+      if (radioBtns && quiz) {
+        radioBtns.forEach((radioBtn) => {
+          quiz.nextQuestion(radioBtn);
+        });
+      }
+      if (mainStartQuiz && oneAnswerQuestions) {
+        mainStartQuiz.classList.add('hidden');
+        oneAnswerQuestions.classList.add('active');
+      }
+      if (confirmBtnHolder && confirmBtn) {
+        confirmBtnHolder.classList.add('active');
+        confirmBtn.classList.add('active');
+      }
+      if (mainHeader) {
+        mainHeader.classList.add('hidden');
+      }
     }
   });
 }());
